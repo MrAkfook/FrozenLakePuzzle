1111acea352f0a0f8cb43093cff3df6382dccef6
DrNielsenn <cagataybirgi@gmail.com>
2024-12-20 15:52:12 +0300

    HazardEquipments

--- src/Equipment/HazardEquipment.java
+++ src/Equipment/HazardEquipment.java
@@ -2,4 +2,22 @@ package Equipment;
 
 public abstract class HazardEquipment extends Equipment{
 
+    public HazardEquipment(int id) {
+        super(id);
+    }
+
+    public HazardEquipment(){
+        super();
+    }
+
+    public HazardEquipment(HazardEquipment other){
+        super(other);
+    }
+
+    @Override
+    public boolean equals(Object other){
+        return super.equals(other);
+    }
+
+
 }
--- src/Equipment/ProtectiveHelmet.java
+++ src/Equipment/ProtectiveHelmet.java
@@ -1,5 +1,27 @@
 package Equipment;
 
+import java.net.ProtocolException;
+
 public class ProtectiveHelmet extends HazardEquipment{
 
+    public ProtectiveHelmet() {
+        super();
+    }
+
+    public ProtectiveHelmet(int id) {
+        super(id);
+    }
+
+    public ProtectiveHelmet(ProtectiveHelmet other){
+        super(other);
+    }
+
+    public boolean equals(Object other){
+        return super.equals(other);
+    }
+
+    @Override
+    public String showOnMap() {
+        return "PH";
+    }
 }
