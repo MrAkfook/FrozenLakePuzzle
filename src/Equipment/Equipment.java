5535fdd99add4687b6e6f3c8dbad8eebb1b1c248
Yılmaz Gökçe <yilmazgokce2010@outlook.com>
2024-12-20 14:54:12 +0300

    Equipment

--- src/Equipment/Equipment.java
+++ src/Equipment/Equipment.java
@@ -1,7 +1,43 @@
 package Equipment;
 
 public abstract class Equipment {
-   
+    private int id;
+
+    public Equipment(int id){
+        this.id = id;
+    } 
+
+    public Equipment(){
+        this(-1);
+    }
+
+    public Equipment(Equipment other){
+        this(other.id);
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public abstract String showOnMap();
+
     @Override
-    public abstract boolean equals();
+    public boolean equals(Object other){
+        // Check if the current object is the same as the passed object
+        if (this == other) {
+            return true;
+        }
+        // Check if the passed object is null or if the classes of the objects are different
+        if (other == null || getClass() != other.getClass()){
+            return false;
+        }
+        else {
+            Equipment otherEquipment = (Equipment) other;
+            return this.id == otherEquipment.id;
+        }      
+    }
 }
