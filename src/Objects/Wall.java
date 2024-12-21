03d9f97a6dbae38bd4703007a3d394c75423fdb0
DrNielsenn <cagataybirgi@gmail.com>
2024-12-22 00:42:22 +0300

    Wall

--- src/Objects/Wall.java
+++ src/Objects/Wall.java
@@ -1,7 +1,32 @@
 package Objects;
 
+import Equipment.HazardEquipment;
 import Interfaces.IMapPlaceable;
 
 public class Wall implements IMapPlaceable{
+
+    int id;
+
+    public Wall(int id) {
+        this.id = id;
+    }
+
+    public Wall(){
+        this.id = 0;
+    }
+
+    public Wall(Wall other){
+        this(other.id);
+    }
+
+    @Override
+    public boolean equals(Object other){
+        return super.equals(other);
+    }
+
+    @Override
+    public String showOnMap() {
+        return ""; 
+    }
 
 }
